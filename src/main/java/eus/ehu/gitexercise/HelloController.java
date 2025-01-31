46b49805090bb790d8421444cd3ea4d1818c2e89
Ivan Gonzalez <igonzalez386@ikasle.ehu.eus>
2025-01-31 09:44:55 +0100

    Ivan adds his name to the code

--- src/main/java/eus/ehu/gitexercise/HelloController.java
+++ src/main/java/eus/ehu/gitexercise/HelloController.java
@@ -10,5 +10,6 @@ public class HelloController {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+        welcomeText.setText("Ivan was here");
     }
 }
