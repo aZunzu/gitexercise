2a439cde6592a11056c6efded9a306df4476c691
Alejandro Zunzunegui Mintegui <azunzunegui003@ikasle.ehu.eus>
2025-01-31 10:31:28 +0100

    Final changes

--- src/main/java/eus/ehu/gitexercise/HelloApplication.java
+++ src/main/java/eus/ehu/gitexercise/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello user!(I changed this to not wait until they edited the file)");
+        stage.setTitle("Hello user!");
         stage.setScene(scene);
         stage.show();
     }
